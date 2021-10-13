5a3e4cf170ace4c89a0f1bdfe59bfd8ff2fa73b5
Opilane TTHK <opilane@tthk.ee>
2021-10-13 10:50:04 +0300

    first commit

---
+++ Week7Arrays/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Week7Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm palub kasutajat valida mida  ta hommikusöögiks sööb
+            string[] food = { "võileib juustuga", "jogurt", "banaan", "peekon", "muna" };
+            Console.WriteLine("Menüüs:");
+            for (int i = 0; i < food.Length; i++)
+            {
+                food[3] = "viinerid";
+                food[1] = "keefir";
+                Console.WriteLine(food[i]);
+            }
+            Console.WriteLine("vali toit(sisesta number 1-5:");
+            int userChoice = Convert.ToInt32(Console.ReadLine()) - 1;
+
+            Console.WriteLine($"Oled valinud {food[userChoice]}");
+        }
+    }
+}
